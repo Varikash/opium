2f74d865e46dfc3a4d4be7804c89577ceef103e7
Nikolay Varikash <n.varicash@gmail.com>
2025-04-15 01:43:28 +0300

    Адаптировал по мобильную версию секцию мастеров

--- src/components/Masters/Masters.tsx
+++ src/components/Masters/Masters.tsx
@@ -1,17 +1,107 @@
 'use client';
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from './Masters.module.css';
 import Image from "next/image";
 import { aboutText } from "@/constants/texts";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+// Импорт стилей Swiper
+import "swiper/css";
+import "swiper/css/pagination";
+
 export default function Masters() {
+
+    // Флаг, отвечающий за отображение карусели для экранов с шириной <= 1256px
+    const [isMobile, setIsMobile] = useState(false);
+
+    // Отслеживаем изменение ширины окна, чтобы переключаться на карусель при нужном размере
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1256);
+        };
+
+        handleResize(); // Устанавливаем начальное значение
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
+    // Собираем данные для мастеров в единый массив для удобства перебора
+    const mastersData = [
+        {
+            image: '/images/Elena.jpeg',
+            alt: 'Elena',
+            about: aboutText.ru.section.masters.elena.about,
+            name: aboutText.ru.section.masters.elena.name,
+            specialization: aboutText.ru.section.masters.elena.specialization,
+        },
+        {
+            image: '/images/Anastasia.jpeg',
+            alt: 'Anastasia',
+            about: aboutText.ru.section.masters.anastasia.about,
+            name: aboutText.ru.section.masters.anastasia.name,
+            specialization: aboutText.ru.section.masters.anastasia.specialization,
+        },
+        {
+            image: '/images/Antonina.jpeg',
+            alt: 'Antonina',
+            about: aboutText.ru.section.masters.antonina.about,
+            name: aboutText.ru.section.masters.antonina.name,
+            specialization: aboutText.ru.section.masters.antonina.specialization,
+        },
+        {
+            image: '/images/Alla.jpeg',
+            alt: 'Alla',
+            about: aboutText.ru.section.masters.alla.about,
+            name: aboutText.ru.section.masters.alla.name,
+            specialization: aboutText.ru.section.masters.alla.specialization,
+        },
+    ];
+
+
+
     return(
         <section
             className={style.main}
         >
             <h2 className={style.header}>{aboutText.ru.section.masters.header}</h2>
             <p className={style.text}>Познакомьтесь с нашей командой <span className={style.decor}>Заботливых</span> экспертов</p>
+            {isMobile ? (
+                    <Swiper
+                        modules={[Pagination]}
+                        grabCursor={true}
+                        allowTouchMove={true}
+                        spaceBetween={1}
+                        slidesPerView={'auto'}
+                        loop={true}
+                        className={style.swiper} // При желании можно добавить собственные стили для Swiper
+                    >
+                        {mastersData.map((master, index) => (
+                            <SwiperSlide key={index} className={style.cardSlide}>
+                                <div className={style.listItem}>
+                                    <div className={style.card}>
+                                        <Image
+                                            src={master.image}
+                                            width={321}
+                                            height={460}
+                                            alt={master.alt}
+                                            className={style.image}
+                                        />
+                                        <div className={style.overlay}>
+                                            <div className={style.line}></div>
+                                            <p className={style.description}>{master.about}</p>
+                                        </div>
+                                    </div>
+                                    <p className={style.cardTitle}>{master.name}</p>
+                                    <span className={style.subTitle}>{master.specialization}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                ) : (
             <ul className={style.list}>
                 <li className={style.listItem}>
                     <div className={style.card}>
@@ -82,7 +172,7 @@ export default function Masters() {
                     <span className={style.subTitle}>{aboutText.ru.section.masters.alla.specialization}</span>
                 </li>
             </ul>
-
+                )}
         </section>
     )
 }
