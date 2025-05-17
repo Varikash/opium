09f08a3d5af97f25bf14351462282302900e3982
Nikolay Varikash <n.varicash@gmail.com>
2025-05-17 18:03:20 +0300

    added background image

--- src/components/MainBody/MainBody.tsx
+++ src/components/MainBody/MainBody.tsx
@@ -1,17 +1,26 @@
 'use client';
 
 import React from "react";
+import Image from "next/image";
 import style from './MainBody.module.css'
 
 interface MainBodyProps {
     children: React.ReactNode;
 }
 
-
 export default function MainBody ({children}: MainBodyProps) {
     return (
         <main className={style.main}>
             {children}
+            <div className={style.backgroundVector}>
+                <Image
+                    src="/images/huve-vector.png"
+                    alt="декор"
+                    width={1200}
+                    height={1300}
+                    priority
+                />
+            </div>
         </main>
     )
 }
